05762a66b42436a7d118ea0e57e257bec6c9f1ee
JJ <jj.spetseris@yahoo.com>
2017-04-27 23:30:03 -0500

    added goblins and sounds

--- catch_the_monster.py
+++ catch_the_monster.py
@@ -23,15 +23,60 @@ clock = pygame.time.Clock()
 def crash_check(mon_pos, hero_pos):
     dist_apart = math.sqrt((mon_pos[0] - hero_pos[0])**2) + math.sqrt((mon_pos[1] - hero_pos[1])**2)
     if dist_apart < 32:
+        pygame.mixer.music.stop()
+        pygame.mixer.music.load('sounds/win.wav')
+        pygame.mixer.music.play()
         return True
     else:
         return False
 
+def goblin_check(goblins, hero_pos):
+    for goblin in goblins:
+        dist_apart = math.sqrt((goblin.pos[0] - hero_pos[0])**2) + math.sqrt((goblin.pos[1] - hero_pos[1])**2)
+        if dist_apart < 32:
+            pygame.mixer.music.stop()
+            pygame.mixer.music.load('sounds/lose.wav')
+            pygame.mixer.music.play()
+            return True
+    else:
+        return False
+
+def menu_screen():
+    while True:
+        screen.fill((97, 159, 182))
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == K_ESCAPE:
+                    sys.exit()
+                if event.key == K_RETURN:
+                    main()
+
+        pygame.font.init()
+        myfont = pygame.font.SysFont('Comic Sans MS', 30)
+        textsurface = myfont.render('Hit ENTER to play.', False, (0, 0, 0))
+        screen.blit(textsurface,(160,200))
+
+        pygame.display.update()
+        clock.tick(60)
+
 def main():
     # create sprites and background
     hero = Hero('images/hero.png')
-    monster = Monster('images/monster.png', 120, 50)
+    monster = Sprite('images/monster.png')
     background = pygame.image.load('images/background.png')
+
+    # builds goblin list
+    goblins = []
+    for i in range(random.randint(2,5)):
+        goblins.append(Goblin('images/goblin.png'))
+
+
+    # load and start music
+    pygame.mixer.music.load('sounds/music.wav')
+    pygame.mixer.music.play(-1)
 
     # initilize variables
     herox = hero.pos[0]
@@ -41,7 +86,7 @@ def main():
     count = -1
 
     # main game loop starts
-    while not crash_check(monster.pos, hero.pos):
+    while True:
         # counts the iterations in the loop and switches movement for monster's
         # random love directions
         count += 1
@@ -85,8 +130,16 @@ def main():
         hero.move(herox, heroy, screen)
         monster.move(switch, screen, WIDTH, HEIGHT)
 
+        for goblin in goblins:
+            goblin.move(switch, screen, WIDTH, HEIGHT)
+
+        if crash_check(monster.pos, hero.pos):
+            menu_screen()
+        if goblin_check(goblins, hero.pos):
+            menu_screen()
+
         pygame.display.update()
         clock.tick(60)
 
 if __name__ == "__main__":
-    main()
+    menu_screen()
--- characters.py
+++ characters.py
@@ -1,11 +1,22 @@
-import pygame
+'''
+To Do:
+    make goblins movement independent from one another
+
+    add lifes and final death when lifes up
+
+    add leader board
+'''
+
+
+import pygame, random
 
 class Sprite:
-    def __init__(self, filename, x=200, y=200):
+    def __init__(self, filename):
         self.img = pygame.image.load(filename)
-        self.pos = [x, y]
+        self.pos = random.choice([[50,50],[50,420],[420,420],[420,50]])
         self.colorkey = [0, 0, 0]
         self.alpha = 255
+        self.speed = 1
 
     def render(self, screen):
         try:
@@ -16,36 +27,31 @@ class Sprite:
             print('An error has occurred while the game was rendering the image.')
             exit(0)
 
-    def move(self, x, y):
-        self.pos = [x, y]
-        self.render(screen)
-
-class Monster(Sprite):
     def move(self, switch, screen, width, height):
         x = self.pos[0]
         y = self.pos[1]
 
         # script for monster's random movement
         if switch == 0:
-            x += 4
+            x += 4 * self.speed
         elif switch == 1:
-            x += 3
-            y += 3
+            x += 3 * self.speed
+            y += 3 * self.speed
         elif switch == 2:
-            x += -3
-            y += 3
+            x += -3 * self.speed
+            y += 3 * self.speed
         elif switch == 3:
-            x += -4
+            x += -4 * self.speed
         elif switch == 4:
-            y += 4
+            y += 4 * self.speed
         elif switch == 5:
-            x += 3
-            y += -3
+            x += 3 * self.speed
+            y += -3 * self.speed
         elif switch == 6:
-            x += -3
-            y += -3
+            x += -3 * self.speed
+            y += -3 * self.speed
         elif switch == 7:
-            y += -4
+            y += -4 * self.speed
 
         # resets monster when it moves off the edge of the screen
         if self.pos[0] > width:
@@ -60,7 +66,27 @@ class Monster(Sprite):
         self.pos = [x, y]
         self.render(screen)
 
+class Goblin(Sprite):
+    def __init__(self, filename):
+        self.img = pygame.image.load(filename)
+        self.pos = random.choice(
+            [[random.randint(20,100), random.randint(20,100)],
+            [random.randint(100,150), random.randint(100,150)],
+            [random.randint(250,325), random.randint(250,325)],
+            [random.randint(325,460), random.randint(325,460)]]
+            )
+        self.colorkey = [0, 0, 0]
+        self.alpha = 255
+        self.speed = random.choice([0.3, 0.5, 0.7])
+
+
 class Hero(Sprite):
+    def __init__(self, filename):
+        self.img = pygame.image.load(filename)
+        self.pos = [200, 200]
+        self.colorkey = [0, 0, 0]
+        self.alpha = 255
+
     def move(self, x, y, screen):
         self.pos = [x, y]
         self.render(screen)
